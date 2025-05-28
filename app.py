62328afc31dbe91f3252b40349c14853ccd2ab46
kinyonyi <45310961+kinyonyi@users.noreply.github.com>
2025-05-28 19:48:36 +0300

    Update app.py
    changed run

--- app.py
+++ app.py
@@ -130,4 +130,5 @@ def handle_frame(data):
 # Run server
 # ────────────────────────────────────────────────
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app)
+    #socketio.run(app, host="0.0.0.0", port=5000, debug=True)
